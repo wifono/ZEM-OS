f21126efa1fa8834a93e38a40d4b78687bad3cf3
wifono <daniel.trstansky@gmail.com>
2022-04-06 14:59:29 +0200

    :)

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,1 +1,15 @@
+const parallaxElements = [...document.getElementsByClassName('services')];
 
+console.log(parallaxElements);
+
+const parallax = function(img) {
+    const speed = 3;
+    let position = '-' + (window.pageYOffset / speed) + "px";
+    img.style.backgroundPosition = `center ${position}`;
+}
+
+window.addEventListener('scroll', function(e) {
+    parallaxElements.forEach((img) => {
+        parallax(img);
+    }) 
+})
